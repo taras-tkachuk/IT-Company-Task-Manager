fa717fbf62f67d38b872d24587a9d6d41f48b296
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-08-15 16:36:30 +0300

    Add models in models.py

--- task/models.py
+++ task/models.py
@@ -1,3 +1,65 @@
+from django.contrib.auth.models import AbstractUser
 from django.db import models
 
-# Create your models here.
+from task_manager import settings
+
+
+class TaskType(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Position(models.Model):
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Worker(AbstractUser):
+    position = models.ForeignKey(Position, on_delete=models.CASCADE)
+
+    def __str__(self) -> str:
+        return f"{self.username} ({self.first_name} {self.last_name})"
+
+
+class Tag(models.Model):
+    name = models.CharField(
+        max_length=55,
+        unique=True
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Task(models.Model):
+    PRIORITY_CHOICES = [
+        ("urgent", "Urgent"),
+        ("high", "High"),
+        ("medium", "Medium"),
+        ("low", "Low"),
+    ]
+
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    deadline = models.DateField()
+    is_completed = models.BooleanField()
+    priority = models.CharField(max_length=9, choices=PRIORITY_CHOICES)
+    task_type = models.ForeignKey(TaskType, on_delete=models.CASCADE)
+    assignees = models.ManyToManyField(
+        settings.AUTH_USER_MODEL,
+        related_name="tasks"
+    )
+    tags = models.ManyToManyField(Tag, related_name="tasks")
+
+    def __str__(self) -> str:
+        return f"{self.name}, deadline: {self.deadline}"
