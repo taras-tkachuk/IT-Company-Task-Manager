ea5d9b55ecb62af405b201db5ebb9577bfaf335e
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-08-15 19:14:51 +0300

    Add info on home page

--- task/views.py
+++ task/views.py
@@ -1,10 +1,16 @@
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
 
+from task.models import Worker, Task, TaskType
+
 
 def index(request: HttpRequest) -> HttpResponse:
+    num_workers = Worker.objects.count()
+    num_tasks = Task.objects.count()
+    num_task_types = TaskType.objects.count()
     context = {
-
+        "num_workers": num_workers,
+        "num_tasks": num_tasks,
+        "num_task_types": num_task_types,
     }
-    # return render(request, "", context=context)
-    return HttpResponse("HELLO")
+    return render(request, "task/index.html", context=context)
