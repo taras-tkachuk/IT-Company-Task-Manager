b2ec199303ccd523cc1a992ff984ec41eff9a885
Taras Tkachuk <tarastkachuk123@gmail.com>
2024-08-15 20:32:53 +0300

    Add view for three main page

--- task/urls.py
+++ task/urls.py
@@ -1,9 +1,12 @@
 from django.urls import path
 
-from task.views import index
+from task.views import index, WorkerListView, TaskListView, TaskTypeListView
 
 urlpatterns = [
-    path("", index, name="index")
+    path("", index, name="index"),
+    path("workers/", WorkerListView.as_view(), name="worker-list"),
+    path("tasks/", TaskListView.as_view(), name="task-list"),
+    path("tasks-types/", TaskTypeListView.as_view(), name="task-type-list"),
 ]
 
 app_name = "task"
--- task/views.py
+++ task/views.py
@@ -1,5 +1,7 @@
+from django.contrib.auth.mixins import LoginRequiredMixin
 from django.http import HttpRequest, HttpResponse
 from django.shortcuts import render
+from django.views import generic
 
 from task.models import Worker, Task, TaskType
 
@@ -14,3 +16,17 @@ def index(request: HttpRequest) -> HttpResponse:
         "num_task_types": num_task_types,
     }
     return render(request, "task/index.html", context=context)
+
+
+class WorkerListView(LoginRequiredMixin, generic.ListView):
+    model = Worker
+
+
+class TaskListView(LoginRequiredMixin,generic.ListView):
+    model = Task
+
+
+class TaskTypeListView(LoginRequiredMixin, generic.ListView):
+    model = TaskType
+    template_name = "task/task_type_list.html"
+    context_object_name = "task_type_list"
